62780c39d55d242b958b7cd2dba405cac485e631
Yuri Krupnik <krupnik.yuri@gmail.com>
2023-11-05 19:39:58 +0200

    added 2 rust apps

--- apps/rust/test_app/src/main.rs
+++ apps/rust/test_app/src/main.rs
@@ -1,20 +1,4 @@
-use actix_web::{web, App, HttpServer, Responder};
-
-async fn index() -> impl Responder {
-  "Hello world!"
+fn main() {
+    println!("Hello, world!");
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-  HttpServer::new(|| {
-    App::new().service(
-      // prefixes all resources and routes attached to it...
-      web::scope("/app")
-        // ...so this handles requests for `GET /app/index.html`
-        .route("/index.html", web::get().to(index)),
-    )
-  })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
-}
